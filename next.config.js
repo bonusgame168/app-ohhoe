00d025e137f5f3a9b7cc33065791b19c2dfc69dd
AlexKhonshu <nopphawong.n@gmail.com>
2024-03-07 16:21:09 +0700

    change port

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  server: {
+    port: 3033
+  },
   reactStrictMode: true,
   images: {
     domains: ['strapi.bonusgame168.com', 'localhost']
