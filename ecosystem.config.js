cb131e68eb07f70ef5a7f34e28a6fbecec4d879e
AlexKhonshu <nopphawong.n@gmail.com>
2024-03-09 01:58:06 +0700

    change port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,10 +5,6 @@ module.exports = {
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
       max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3033,
-      }
     }
   ]
 }
